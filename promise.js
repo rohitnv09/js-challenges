3296f0674b809022a2cbc8cb7c435b0260aed673
Rohit Sharma <rohitshrm0902@gmail.com>
2026-06-10 23:55:19 +0530

    runCallbacks fixes

--- promise.js
+++ promise.js
@@ -17,6 +17,20 @@ class CustomPromise {
             this.#bindedReject(error);
         }
     }
+    #runCallbacks() {
+        if (this.#state === PROMISE_STATES.FULFILLED) {
+            this.#thenCbs.forEach((cb) => {
+                cb(this.#result)
+            });
+            this.#thenCbs = [];
+        }
+        if (this.#state === PROMISE_STATES.FULFILLED) {
+            this.#catchCbs.forEach((cb) => {
+                cb(this.#result)
+            });
+            this.#catchCbs = [];
+        }
+    }
     #resolve(value) {
         if (this.#state !== PROMISE_STATES.PENDING) return;
         if (value instanceof CustomPromise) {
@@ -24,11 +38,13 @@ class CustomPromise {
         }
         this.#state = PROMISE_STATES.FULFILLED;
         this.#result = value;
+        this.#runCallbacks();
     }
     #reject() {
         if (this.#state !== PROMISE_STATES.PENDING) return;
         this.#state = PROMISE_STATES.REJECTED;
         this.#result = value;
+        this.#runCallbacks();
     }
     static resolve(value) {
         return new CustomPromise((resolve) => resolve(value));
@@ -37,12 +53,42 @@ class CustomPromise {
         return new CustomPromise((_, reject) => reject(value));
     }
     then(successCb, failureCb) {
-
+        return new CustomPromise((resolve, reject) => {
+            this.#thenCbs.push((result) => {
+                if (successCb == null) {
+                    resolve(result);
+                    return;
+                }
+                try {
+                    resolve(successCb(result));
+                } catch (error) {
+                    reject(error);
+                }
+            });
+            this.#catchCbs.push((result) => {
+                if (failureCb == null) {
+                    reject(result);
+                    return;
+                }
+                try {
+                    resolve(successCb(result));
+                } catch (error) {
+                    reject(error);
+                }
+            });
+        });
+        this.#runCallbacks();
     }
     catch(cb) {
         return this.then(null, cb);
     }
     finally(cb) {
-        return this.then();
+        return this.then((result) => {
+            cb();
+            return result;
+        }, (reason) => {
+            cb();
+            throw reason;
+        });
     }
 }
